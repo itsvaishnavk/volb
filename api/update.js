146f1c94cd5dd3aabd67ac77a669fdb091ce026b
itsvaishnavk <its.vaishnav.k@gmail.com>
2026-04-19 14:51:22 +0530

    Update update.js

--- api/update.js
+++ api/update.js
@@ -2,7 +2,7 @@ export default function handler(req, res) {
   res.status(200).json({
     app: "VOLB",
     version: "1.0.0",
-    size: "5 MB",
+    size: "1.77 MB",
     download: "/apk/volb.apk",
     official: "https://volb.vercel.app"
   });
